b6321f19a0fafaa00e3751c945ad800a6b5e0ab9
IceFox2024 <168815657+IceFox2024@users.noreply.github.com>
2024-06-05 20:43:07 +0200

    Update LineSensors.cpp

--- LineSensors.cpp
+++ LineSensors.cpp
@@ -1,8 +1,11 @@
 #include "LineSensors.h"
 #include <Zumo32U4.h>
 
+// Maak een object aan voor de Zumo32U4ButtonB en Zumo32U4LineSensors
 Zumo32U4ButtonB buttonB;
+Zumo32U4ButtonA buttonA;
 Zumo32U4LineSensors lineSensors;
+Zumo32U4Motors motors;
 
 // initialiseer 3 sensoren
 LineSensors::LineSensors() : LaatsteGelezenUitvoer(0) {
@@ -13,10 +16,18 @@ LineSensors::LineSensors() : LaatsteGelezenUitvoer(0) {
 
 // Setup methode voor het initialiseren van de sensoren en kalibratie
 void LineSensors::startup() {
-    lineSensors.initThreeSensors();
-    pinMode(5, OUTPUT);
-    calibrateLineSensor();
-    Serial.begin(9600);
+  if (status==0){
+    if (buttonA.getSingleDebouncedPress()){
+      Serial.println("Knop A is ingedrukt!");
+      delay(1000);
+      lineSensors.initThreeSensors();
+      pinMode(5, OUTPUT); // Zet groene led op pin 5 op output
+      calibrateLineSensor();
+      Serial.begin(9600);
+      status=1;
+      Serial.println(status);
+    }
+  }
 }
 
 // Loop methode voor het lezen en printen
@@ -27,6 +38,7 @@ int LineSensors::checkWaardesLS() {
         printReadingsToSerial(); 
     }
     if (buttonB.isPressed()){
+        delay(1000);
         calibrateLineSensor();
     }
 }
@@ -34,10 +46,13 @@ int LineSensors::checkWaardesLS() {
 // Methode om de lijnsensoren te kalibreren
 int LineSensors::calibrateLineSensor() {
     digitalWrite(5, LOW); // Zet de groene LED uit
-    for (uint16_t i = 0; i < 200; i++) {
-        lineSensors.calibrate(); // Kalibreer de sensoren 200 keer
+    for (uint16_t i = 0; i < 400; ++i) {
+        lineSensors.calibrate(); // Kalibreer de sensoren 400 keer
+        motors.setSpeeds(150, -165); // Vooruit met links, achteruit met rechts voor draaien
+        delay(10); // Korte delay voor een vloeiende beweging
     }
     digitalWrite(5, HIGH); // Zet de groene LED aan
+    motors.setSpeeds(0, -0);
     return 0;
 }
 
@@ -47,7 +62,8 @@ int LineSensors::calibrateLineSensor() {
 void LineSensors::printReadingsToSerial() {
     Serial.print("Line sensoren output: ");
     for (uint8_t i = 0; i < 3; i++) {
-        Serial.print(lineSensorValues[i]); 
+        int mappedValue = map(lineSensorValues[i], 0, 1000, 0, 10000);
+        Serial.print(mappedValue);
         Serial.print(" ");
     }
     Serial.println(); 
@@ -64,6 +80,8 @@ int LineSensors::checkLijnen() {
             DonkersteSensor = i;
         }
     }
+        Serial.print(" Dit is de donkerste sensor : ");
+        Serial.print(DonkersteSensor);
     return DonkersteSensor;
 }
 
@@ -81,27 +99,50 @@ int LineSensors::berekenFout() {
 
     return DonkersteSensor;
 }
+// int LineSensors::KleurCheck() {
+//   for (int i = 0; i < 3; ++i) {
+//         if (lineSensorValues[i] > 50 && lineSensorValues[i] < 950) {
+//             LSKleur = i;
+//         }
+//     } 
 int LineSensors::KleurCheck() {
   for (int i = 0; i < 3; ++i) {
-        if (lineSensorValues[i] > 50 && lineSensorValues[i] < 950) {
-            LSKleur = i;
-        }
-    } 
-    // Nog geen idee van de waarden voor kleuren
-    if (LSKleur > 50 && LSKleur < 200) {
+        if (i == 0) {
+        lineSensorValues[i];
+        LSKleur = i;
+}
+        if (i == 1) {
+        lineSensorValues[i];
+        LSKleur1 = i;
+}
+        if (i == 2) {
+        lineSensorValues[i];
+        LSKleur2 = i;
+}
+}
+}
+
+    void LineSensors::LSrouteVersturen() {
+    if (LSKleur1 > 600 && LSKleur1 < 1100) {
       //Zumo32U4.rijd_vooruit(---);
+      //Groen
     }
-    if (LSKleur > 200 && LSKleur < 300) {
+    if (LSKleur > 2400 && LSKleur < 2700) {
       //Zumo32U4.rijd_vooruit(---);
+      //Grijs voor Links
     }
-    if (LSKleur > 400 && LSKleur < 500) {
+    if (LSKleur2 > 2100 && LSKleur2 < 2500) {
       //Zumo32U4.rijd_vooruit(---);
+      // Grijs voor Rechts
     }
-    if (LSKleur > 600 && LSKleur < 700) {
+    if (LSKleur > 2700 && LSKleur < 4500) {
       //Zumo32U4.rijd_vooruit(---);
+      //Bruin voor Links
     }
-    if (LSKleur > 800 && LSKleur < 950) {
+    if (LSKleur2 > 2700 && LSKleur2 < 4500) {
       //Zumo32U4.rijd_vooruit(---);
+      //Bruin voor Rechts
     }
 }
+
 
