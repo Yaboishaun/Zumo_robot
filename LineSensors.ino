a4eef16e264d2f67222a32473c15e15ec5f0c914
IceFox2024 <168815657+IceFox2024@users.noreply.github.com>
2024-06-05 20:43:39 +0200

    Update LineSensors.ino

--- LineSensors.ino
+++ LineSensors.ino
@@ -5,8 +5,10 @@ LineSensors myLineSensors;
 
 void setup() {
     myLineSensors.startup(); // Roep de setup methode van het object aan
+    
 }
 
 void loop() {
     myLineSensors.checkWaardesLS(); // Roep de loop methode van het object aan
+    myLineSensors.startup();
 }
